6d437b408173eac1ca20ab0155d843f2373ca00f
andrew.howe <andrew.howe@rakuten.com>
2018-11-22 16:55:49 +0900

    keep track of standard radicals, move data to hashref

--- src/generate.pl
+++ src/generate.pl
@@ -1,26 +1,28 @@
 #!/usr/local/bin/perl
 
-# TODO: exclude js02 and other non-standard stroke numbers
-
 # 次のようなファーマットのJSON形式に変換する
 # {
 #   "一": {
 #     "strokes": 1,
+#     "standard": true,
 #     "kanji": ["丂", "丄", "丅", "丌", "丒", ...]
 #   },
 #   ...
 # }
+#
+# strokes = 画数（整数）
+# standard = 通常の部首かどうか（ブーリアン型）
+# kanji = この部首を含む漢字（配列）
 
 use strict;
 use warnings;
 
+die "Usage: generate.pl path/to/radkfile\n" unless $ARGV[0];
 open(my $radkfile, '<:encoding(euc-jp)', $ARGV[0]) or die "Could not open $ARGV[0]\n";
 open(my $output, '>:encoding(utf-8)', 'output.json') or die "Could not open output.json for writing\n";
 
 sub main {
-  my $currentRadical = undef;
-  my $numStrokes = undef;
-  my @relatedKanji = undef;
+  my $radicalData = undef;
 
   print $output '{';
 
@@ -28,35 +30,45 @@ sub main {
     next if $line =~ /^#/; # skip comments
 
     if ($line =~ /^\$/) {
-      outputRadical($currentRadical, $numStrokes, @relatedKanji, 0);
+      outputRadical($radicalData, 0);
 
-      my @matches = ($line =~ /^\$ (.) (\d+)/);
+      my @matches = ($line =~ /^\$ (.) (\d+)(.+)?$/);
 
-      die "Error while parsing $line\n" if !@matches || @matches != 2;
+      die "Error while parsing $line\n" if !@matches || @matches < 2;
 
-      ($currentRadical, $numStrokes) = @matches;
-      @relatedKanji = ();
+      $radicalData = makeRadicalData(@matches);
     } else {
       chomp($line);
-      push(@relatedKanji, split('', $line));
+      push(@{$radicalData->{relatedKanji}}, split('', $line));
     }
   }
 
-  outputRadical($currentRadical, $numStrokes, @relatedKanji, 1);
+  outputRadical($radicalData, 1);
 
   print $output '}';
 }
 
-sub outputRadical {
-  my $isLast = pop(@_);
-  my ($radical, $strokes, @kanji) = @_;
+sub makeRadicalData {
+  return {
+    radical      => $_[0],
+    numStrokes   => $_[1],
+    isStandard   => ! defined $_[2],
+    relatedKanji => [],
+  };
+}
 
-  return unless $radical;
+sub outputRadical {
+  my ($radicalData, $isLast) = @_;
+
+  return unless $radicalData;
+
+  my @kanji = @{$radicalData->{relatedKanji}};
 
   print $output sprintf(
-    '"%s":{"strokes":%d,"kanji":[%s]}',
-    $radical,
-    $strokes,
+    '"%s":{"strokes":%d,"standard":%s,"kanji":[%s]}',
+    $radicalData->{radical},
+    $radicalData->{numStrokes},
+    $radicalData->{isStandard} ? 'true' : 'false',
     @kanji ? '"' . join('","', @kanji) . '"' : ''
   );
 
