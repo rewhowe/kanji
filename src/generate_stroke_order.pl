36344b4f5ac33d16814dbd76590004bb15685556
andrew.howe <andrew.howe@rakuten.com>
2019-12-04 19:01:04 +0900

    generate stroke order

---
+++ src/generate_stroke_order.pl
@@ -0,0 +1,67 @@
+#!/usr/local/bin/perl
+
+# 次のようなファーマットのJSON形式に変換する
+# [
+#   "一",
+#   "丨",
+#   "丶",
+#   "丿"
+#   ...
+# ]
+#
+# 並び替え：画数が少ない順
+
+use strict;
+use warnings;
+
+use LWP::Simple qw(get);
+
+die "Usage: $0 url_of_strokes_csv\n" unless $ARGV[0];
+open(my $output, '>:encoding(utf-8)', 'stroke_order.json') or die "Could not open stroke_order.json for writing\n";
+
+sub main {
+  my $response = get($ARGV[0]);
+
+  my @kanji = parseResponse($response);
+
+  my @sortedKanji = sort {
+    $a->{strokes} <=> $b->{strokes} ||
+    $a->{kanji}   cmp $b->{kanji}
+  } @kanji;
+
+  outputJson(@sortedKanji);
+}
+
+sub parseResponse {
+  my $response = shift;
+
+  my @kanjiLines = split("\n", $response);
+
+  my @kanji = ();
+  foreach my $line (@kanjiLines) {
+    my @matches = ($line =~ /(.)\s+(\d+)/);
+
+    die "Error while parsing $line\n" if !@matches || @matches != 2;
+
+    push(@kanji, {kanji => $matches[0], strokes => $matches[1]});
+  }
+
+  return @kanji;
+}
+
+sub outputJson {
+  my @sortedKanji = @_;
+
+  print $output '[';
+
+  my $numKanji = @sortedKanji;
+
+  for (my $i = 0; $i < $numKanji; $i++) {
+    print $output "\"$sortedKanji[$i]->{kanji}\"";
+    print $output ',' unless ($i + 1) == $numKanji;
+  }
+
+  print $output ']';
+}
+
+main();
