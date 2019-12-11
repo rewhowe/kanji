d25049b7de37e729a782384649f0adbf8c2a30c7
rewhowe <rewhowe@gmail.com>
2019-12-11 14:16:30 +0900

    Feature/vue components (#1)
    * move candidate list to vue component
    
    * move radical selection to component; fix loading spinner logic
    
    * move sort options to component
    
    * move sort options to an array
    
    * move html contents to js to delay mounting
    
    * move initialization into vue lifecycle hooks
    
    * move all of the global functions into the app vue object
    
    * remove raw data files

--- src/js/app.js
+++ src/js/app.js
@@ -3,212 +3,262 @@
 // * compile js / css
 
 const RADICALS_JSON_URL = 'https://rewhowe.github.io/kanji/public/json/radicals.json';
-const COLLOCATIONS_JSON_URL = 'https://rewhowe.github.io/kanji/public/json/collocations.json';
-
 let RADICAL_MAPPING = [];
+
+const COLLOCATIONS_JSON_URL = 'https://rewhowe.github.io/kanji/public/json/collocations.json';
 let COLLOCATIONS = [];
 
 const app = new Vue({
   el: '#app',
   data: {
     input: '',
-    candidates: [],
+
     include_similar: false,
-    radical_selection: undefined,
+    sort_options: SORT_OPTIONS,
     sort: 'stroke',
-    ready: false,
+
+    candidates: [],
+    radical_selection: undefined,
+
     searching: false,
   },
+  template: `
+    <div class="app">
+      <div>
+        <input type="text" v-model="input" v-on:change="lookup">
+      </div>
+
+      <ul>
+        <li>
+          <input id="include_similar" type="checkbox" v-model="include_similar" v-on:change="lookup">
+          <label for="include_similar">似てる部首も含む&#x1F441;</label>
+        </li>
+        <li>
+          並び替え
+          <sort-option v-for="(label, order) in sort_options"
+                       v-bind:key="order"
+                       v-bind:order="order"
+                       v-bind:label="label"
+                       v-bind:sort="sort"
+                       v-on:sort-candidates="sortCandidates"></sort-option>
+        </li>
+      </ul>
+
+      <candidate-list v-bind:candidates="candidates" v-bind:searching="searching"></candidate-list>
+
+      <radical-selection v-bind:radical_selection="radical_selection"
+                         v-on:select-radical="selectRadical"></radical-selection>
+    </div>
+  `,
+
   methods: {
     lookup: function () {
-      app.searching = true;
+      const self = this;
+      self.searching = true;
 
-      const radicals = getRadicals();
-      const candidates = getCandidates(radicals);
+      // Delay so that loading spinner can render
+      window.setTimeout(function () {
+        const radicals = self.getRadicals();
+        const candidates = self.getCandidates(radicals);
 
-      updateSelection();
+        self.updateSelection();
 
-      sortBy(candidates, app.sort, candidates => app.candidates = candidates);
+        sortBy(candidates, self.sort, candidates => self.candidates = candidates);
 
-      app.searching = false;
+        self.searching = false;
+      }, 1);
     },
 
     selectRadical: function (radical) {
-      app.searching = true;
+      const self = this;
+      self.searching = true;
 
-      const radical_data = RADICAL_MAPPING[radical];
-      const is_selected = app.radical_selection[radical_data.strokes][radical].selected;
-      const is_available = app.radical_selection[radical_data.strokes][radical].available;
+      // Delay so that loading spinner can render
+      window.setTimeout(function () {
+        const radical_data          = RADICAL_MAPPING[radical];
+        const is_currently_selected = self.radical_selection[radical_data.strokes][radical].selected;
+        const is_available          = self.radical_selection[radical_data.strokes][radical].available;
 
-      if (!is_available && !is_selected) return;
+        if (!is_available && !is_currently_selected) return;
 
-      const display_radical = RADK_DISPLAY[radical] || radical;
-      app.input = app.input.replace(new RegExp(display_radical, 'g'), '');
-      if (!is_selected) {
-        app.input = app.input + display_radical;
-      }
+        const display_radical = RADK_DISPLAY[radical] || radical;
+        self.input = self.input.replace(new RegExp(display_radical, 'g'), '');
+        if (!is_currently_selected) {
+          self.input = self.input + display_radical;
+        }
 
-      app.lookup();
+        self.lookup();
+      }, 1);
     },
 
-    sortCandidates: function () {
-      app.searching = true;
-      sortBy(app.candidates, app.sort, candidates => app.candidates = candidates);
-      app.searching = false;
+    sortCandidates: function (order) {
+      const self = this;
+      self.sort = order;
+      self.searching = true;
+      sortBy(self.candidates, self.sort, candidates => self.candidates = candidates);
+      self.searching = false;
     },
-  },
-});
 
-function getRadicals() {
-  app.input = app.input.replace(/[!！][^!！]/g, s => ALTERNATE_FORMS[s[1]] || s[1]);
+    initialiseRadicalSelection: function (is_all_available) {
+      const self = this;
+      const radical_selection = {};
 
-  return [...app.input].map(function (radical) {
-    return (app.include_similar && LOOKALIKES[radical]) || [RADK[radical] || radical];
-  });
-}
+      Object.keys(RADICAL_MAPPING).forEach(function (radical) {
+        const radical_data = RADICAL_MAPPING[radical];
 
-function getCandidates(radicals) {
-  if (radicals.length == 0) return [];
+        if (!radical_selection[radical_data.strokes]) radical_selection[radical_data.strokes] = {};
 
-  let candidates;
+        radical_selection[radical_data.strokes][radical] = {
+          display: RADK_DISPLAY[radical] || radical,
+          selected: false,
+          lookalike_selected: false,
+          available: is_all_available,
+        };
+      });
 
-  radicals.forEach(function (radical) {
-    if (candidates && candidates.length == 0) return;
+      self.radical_selection = radical_selection;
+    },
 
-    const kanji = getKanjiWithRadical(radical);
+    getRadicals: function () {
+      const self = this;
+      self.input = self.input.replace(/[!！][^!！]/g, s => ALTERNATE_FORMS[s[1]] || s[1]);
 
-    if (candidates === undefined) {
-      candidates = kanji;
-    } else {
-      candidates = intersect(candidates, kanji);
-    }
-  });
+      return [...self.input].map(function (radical) {
+        return (self.include_similar && LOOKALIKES[radical]) || [RADK[radical] || radical];
+      });
+    },
 
-  return [...new Set(candidates)];
-}
+    getCandidates: function (radicals) {
+      if (radicals.length == 0) return [];
 
-function getKanjiWithRadical(radical) {
-  if (radical instanceof Array) return radical.map(getKanjiWithRadical).flat();
+      const self = this;
+      let candidates;
 
-  return RADICAL_MAPPING[radical] ? RADICAL_MAPPING[radical].kanji : [];
-}
+      radicals.forEach(function (radical) {
+        if (candidates && candidates.length == 0) return;
 
-function updateSelection() {
-  const input = [...app.input];
-  const available_radicals = getAvailableRadicals(input);
+        const kanji = self.getKanjiWithRadical(radical);
 
-  initialiseRadicalSelection(available_radicals === undefined);
-
-  input.forEach(function (radical) {
-    setSelection(radical, 'selected');
-
-    if (app.include_similar && LOOKALIKES[radical]) {
-      LOOKALIKES[radical].forEach(r => setSelection(r, 'lookalike_selected'));
-    }
-  });
-
-  if (available_radicals === undefined) return;
-
-  available_radicals.forEach(function (radical) {
-    setSelection(radical, 'available');
-  });
-}
-
-function getAvailableRadicals(input) {
-  let available_radicals;
-  const input_radicals = [];
-
-  input.forEach(function (radical) {
-    radical = RADK[radical] || radical;
-
-    if (!COLLOCATIONS[radical]) return;
-
-    input_radicals.push([radical]);
-
-    // lookup by 1 radical
-    // 一 -> ノ, 人, 丶, 九, 口, 自, マ, 丷, 女, 大, ...
-    if (input_radicals.length == 1) {
-      available_radicals = COLLOCATIONS[radical];
-
-    // lookup by 2 radical pair
-    // 一人 -> ノ, 丶, 口, マ, 丷, 女, 大, ...
-    } else if (input_radicals.length == 2) {
-      const pair = input_radicals.sort().join('');
-      available_radicals = COLLOCATIONS[pair] || [];
-
-    // brute force: compare possible kanji of current input and possible kanji of radicals for next radical
-    //
-    // 3 case
-    // input: 一人口
-    // (available_kanji)   一人  = 関, 病, 復, 春, 券, 臨, 傷, 咲, 奉, ...
-    // (possible_radicals) 一口 -> ｜, 羊, 阡, 女, 个, 心, 丷, 女, 大
-    // (possible_kanji 1)  ｜    = 中, 出, 東, 業, 選, 円, 奉, ... -> available (common: 奉, ...)
-    // (possible_kanji 2)  羊    = 業, 鮮, 着, 義, 差, 美, ...     -> unavailable (no common kanji)
-    // ...
-    // (possible_kanji 7)  丷    = 前, 業, 新, 金, 関, 咲, ...     -> available (common: 関, 咲, ...)
-    // ...
-    //
-    // 4 case
-    // input: 一人口丷
-    // (available_kanji)   一人口 = 塩, 咲, 倹, 僉, 儉, ...
-    // (possible_radicals) 一丷  -> 皿, ハ, 冂, 山, 大
-    // (possible_kanji 1)  皿     = 盟, 益, 盛, 血, 温, ... -> unavailable (no common kanji)
-    // ...
-    // (possible_kanji 5)  大     = 大, 実, 決, 勝, 咲, ... -> available (common: 咲)
-    // ...
-    } else {
-      available_radicals = [];
-      // get possible radicals via pair (to reduce the possibilies)
-      const next_pair = [input_radicals[0], [radical]].sort().join('');
-      const possible_radicals = COLLOCATIONS[next_pair] || [];
-
-      const available_kanji = getCandidates(input_radicals);
-
-      possible_radicals.forEach(function (r) {
-        const possible_kanji = RADICAL_MAPPING[r].kanji;
-        if (intersect(available_kanji, possible_kanji).length > 0) {
-          available_radicals.push(r);
+        if (candidates === undefined) {
+          candidates = kanji;
+        } else {
+          candidates = intersect(candidates, kanji);
         }
       });
-    }
-  });
 
-  return available_radicals;
-}
+      return [...new Set(candidates)];
+    },
 
-function setSelection(radical, property) {
-  radical = RADK[radical] || radical;
-  const radical_data = RADICAL_MAPPING[radical];
-  if (radical_data) app.radical_selection[radical_data.strokes][radical][property] = true;
-}
+    getKanjiWithRadical: function (radical) {
+      const self = this;
+      if (radical instanceof Array) return radical.map(self.getKanjiWithRadical).flat();
 
-function initialiseRadicalSelection(is_all_available) {
-  const radical_selection = {};
+      return RADICAL_MAPPING[radical] ? RADICAL_MAPPING[radical].kanji : [];
+    },
 
-  Object.keys(RADICAL_MAPPING).forEach(function (radical) {
-    const radical_data = RADICAL_MAPPING[radical];
+    updateSelection: function () {
+      const self = this;
+      const input = [...self.input];
+      const available_radicals = self.getAvailableRadicals(input);
 
-    if (!radical_selection[radical_data.strokes]) radical_selection[radical_data.strokes] = {};
+      self.initialiseRadicalSelection(available_radicals === undefined);
 
-    radical_selection[radical_data.strokes][radical] = {
-      display: RADK_DISPLAY[radical] || radical,
-      selected: false,
-      lookalike_selected: false,
-      available: is_all_available,
-    };
-  });
+      input.forEach(function (radical) {
+        self.setSelection(radical, 'selected');
 
-  app.radical_selection = radical_selection;
-}
+        if (self.include_similar && LOOKALIKES[radical]) {
+          LOOKALIKES[radical].forEach(r => self.setSelection(r, 'lookalike_selected'));
+        }
+      });
 
-getJson(RADICALS_JSON_URL, function (data) {
-  RADICAL_MAPPING = data;
+      if (available_radicals === undefined) return;
 
-  initialiseRadicalSelection(true);
+      available_radicals.forEach(function (radical) {
+        self.setSelection(radical, 'available');
+      });
+    },
 
-  getJson(COLLOCATIONS_JSON_URL, function (data) {
-    COLLOCATIONS = data;
+    setSelection: function (radical, property) {
+      const self = this;
+      radical = RADK[radical] || radical;
+      const radical_data = RADICAL_MAPPING[radical];
+      if (radical_data) self.radical_selection[radical_data.strokes][radical][property] = true;
+    },
 
-    app.ready = true;
-  });
+    getAvailableRadicals: function (input) {
+      const self = this;
+      const input_radicals = [];
+      let available_radicals;
+
+      input.forEach(function (radical) {
+        radical = RADK[radical] || radical;
+
+        if (!COLLOCATIONS[radical]) return;
+
+        input_radicals.push([radical]);
+
+        // lookup by 1 radical
+        // 一 -> ノ, 人, 丶, 九, 口, 自, マ, 丷, 女, 大, ...
+        if (input_radicals.length == 1) {
+          available_radicals = COLLOCATIONS[radical];
+
+        // lookup by 2 radical pair
+        // 一人 -> ノ, 丶, 口, マ, 丷, 女, 大, ...
+        } else if (input_radicals.length == 2) {
+          const pair = input_radicals.sort().join('');
+          available_radicals = COLLOCATIONS[pair] || [];
+
+        // brute force: compare possible kanji of current input and possible kanji of radicals for next radical
+        //
+        // 3 case
+        // input: 一人口
+        // (available_kanji)   一人  = 関, 病, 復, 春, 券, 臨, 傷, 咲, 奉, ...
+        // (possible_radicals) 一口 -> ｜, 羊, 阡, 女, 个, 心, 丷, 女, 大
+        // (possible_kanji 1)  ｜    = 中, 出, 東, 業, 選, 円, 奉, ... -> available (common: 奉, ...)
+        // (possible_kanji 2)  羊    = 業, 鮮, 着, 義, 差, 美, ...     -> unavailable (no common kanji)
+        // ...
+        // (possible_kanji 7)  丷    = 前, 業, 新, 金, 関, 咲, ...     -> available (common: 関, 咲, ...)
+        // ...
+        //
+        // 4 case
+        // input: 一人口丷
+        // (available_kanji)   一人口 = 塩, 咲, 倹, 僉, 儉, ...
+        // (possible_radicals) 一丷  -> 皿, ハ, 冂, 山, 大
+        // (possible_kanji 1)  皿     = 盟, 益, 盛, 血, 温, ... -> unavailable (no common kanji)
+        // ...
+        // (possible_kanji 5)  大     = 大, 実, 決, 勝, 咲, ... -> available (common: 咲)
+        // ...
+        } else {
+          available_radicals = [];
+          // get possible radicals via pair (to reduce the possibilies)
+          const next_pair = [input_radicals[0], [radical]].sort().join('');
+          const possible_radicals = COLLOCATIONS[next_pair] || [];
+
+          const available_kanji = self.getCandidates(input_radicals);
+
+          possible_radicals.forEach(function (r) {
+            const possible_kanji = RADICAL_MAPPING[r].kanji;
+            if (intersect(available_kanji, possible_kanji).length > 0) {
+              available_radicals.push(r);
+            }
+          });
+        }
+      });
+
+      return available_radicals;
+    },
+  },
+
+  beforeMount: function () {
+    getJson(RADICALS_JSON_URL, function (data) {
+      RADICAL_MAPPING = data;
+    });
+    getJson(COLLOCATIONS_JSON_URL, function (data) {
+      COLLOCATIONS = data;
+    });
+  },
+
+  mounted: function () {
+    this.initialiseRadicalSelection(true);
+  },
 });
---
+++ src/js/components/candidate_list.js
@@ -0,0 +1,18 @@
+Vue.component('candidate-list', {
+  props: {
+    searching: Boolean,
+    candidates: Array,
+  },
+  template: `
+    <div class="candidate-list-container">
+      <i v-if="searching" v-model="searching" class="loading"></i>
+      <div class="candidate-list">
+        <span v-if="!searching"
+              v-for="candidate in candidates"
+              v-bind:key="candidate"
+              class="candidate">{{ candidate }}</span>
+      </div>
+      <div class="more"></div>
+    </div>
+  `,
+});
