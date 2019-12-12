4c83a8a2860c740db91ce132aa56f311f34e15f7
andrew.howe <andrew.howe@rakuten.com>
2019-12-12 17:00:44 +0900

    change help modal to about section; very minor styling

--- src/js/app.js
+++ src/js/app.js
@@ -48,10 +48,7 @@ const app = new Vue({
       <radical-selection v-bind:radical_selection="radical_selection"
                          v-on:select-radical="selectRadical"></radical-selection>
 
-      <div class="footer">
-        <help-modal></help-modal>
-        <a class="footer-link" href="https://github.com/rewhowe/kanji">GitHub</a>
-      </div>
+      <about-section></about-section>
     </div>
   `,
 
