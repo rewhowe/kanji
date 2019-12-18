983c4eae108c25376bb121e6f68fbe52c89421d8
andrew.howe <andrew.howe@rakuten.com>
2019-12-18 13:32:40 +0900

    hide drop down on outside click; remove old console logs

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -4,10 +4,8 @@ const LOCAL_STORAGE_AVAILABLE = (() => { try { return !!localStorage; } catch (e
 
 function getJson(url, callback) {
   if (LOCAL_STORAGE_AVAILABLE && localStorage[url]) {
-    console.log('fetch ' + url + ' from cache');
     callback(JSON.parse(localStorage[url]));
   } else {
-    console.log('get ' + url + ' from internet');
     axios.get(url)
     .then(function (response) {
       if (LOCAL_STORAGE_AVAILABLE) localStorage[url] = JSON.stringify(response.data);
